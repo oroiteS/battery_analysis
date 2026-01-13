1ee91a614ed5f9213b7c2c8218f47a6133e2c79d
unknown <2801611461@qq.com>
2026-01-13 14:08:37 +0800

    feat:增加了analysis相关的前端api

---
+++ frontend/src/api/analysis.ts
@@ -0,0 +1,32 @@
+import service from './index'
+import type {
+  FeatureStat,
+  TrendData,
+  ScatterData,
+  PCLDistribution,
+  CorrelationMatrix
+} from './types'
+
+export const getBatteryStats = (batteryId: number) => {
+  return service.get<any, FeatureStat[]>(`/v1/data/batteries/${batteryId}/stats`)
+}
+
+export const getTrendData = (batteryId: number, featureName: string) => {
+  return service.get<any, TrendData>(`/v1/data/batteries/${batteryId}/trend`, {
+    params: { feature_name: featureName }
+  })
+}
+
+export const getScatterData = (batteryId: number, featureName: string) => {
+  return service.get<any, ScatterData>(`/v1/data/batteries/${batteryId}/scatter`, {
+    params: { feature_name: featureName }
+  })
+}
+
+export const getPCLDistribution = (batteryId: number) => {
+  return service.get<any, PCLDistribution>(`/v1/data/batteries/${batteryId}/pcl-distribution`)
+}
+
+export const getCorrelationMatrix = (batteryId: number) => {
+  return service.get<any, CorrelationMatrix>(`/v1/data/batteries/${batteryId}/correlation-matrix`)
+}
